2bdbed575268c57ebed54d0b59b25f57216e9131
Howard Fan <shih-hao_fan@troweprice.com>
2018-01-15 19:48:19 -0500

    added thymeleaf and modelview into the project

--- src/main/java/com/taxtoken/controller/UserController.java
+++ src/main/java/com/taxtoken/controller/UserController.java
@@ -4,26 +4,33 @@ import com.taxtoken.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.taxtoken.service.UserService;
 
-import java.util.List;
-
-@RestController
-public class Controller {
+@Controller
+public class UserController {
 
     @Autowired
     UserService userService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 
+
     @RequestMapping(method = RequestMethod.GET, path = "/user")
-    @ResponseBody
-    public List<User> getUser() {
-        return userService.getAllUser();
+    public String user(Model model) {
+        model.addAttribute("users", userService.getAllUser());
+        return "user";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/user/{name}")
+    public String user(@PathVariable String name, Model model) {
+        model.addAttribute("users", userService.getUser(name));
+        return "user";
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{name}/birthday")
